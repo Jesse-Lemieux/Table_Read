f9a1fc91908a1f4a60db13e0a225c8043b3bfd22
JESSE <dtrsjesse121@gmail.com>
2022-07-23 20:26:33 -0400

    add more style to ViewImageScreen.js. add more components

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native';
 import AppButton from './app/components/AppButton';
+import Card from './app/components/Card';
 
 
 import AppText from './app/components/AppText';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
   return (
-    <WelcomeScreen />
+    <ViewImageScreen />
+    
   );
 }
