329478c77c87b5f8f39d0f9f17a1e688cb1a77dd
JESSE <dtrsjesse121@gmail.com>
2022-07-25 16:26:17 -0400

    modularize text style and color pallet

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
+import Screen from './app/components/Screen';
+import { TextInput, Text } from 'react-native';
+import { useState } from 'react';
+import AppTextInput from './app/components/AppTextInput';
+
+
+
 
 export default function App() {
-  return <ListingsScreen />
-  ;
+  const [firstName, setFirstName] = useState('')
+  return (
+    <Screen>
+      <AppTextInput placeholder="Username" icon="email"/>
+    </Screen>
+  );
 }
---
+++ app/config/styles.js
@@ -0,0 +1,20 @@
+import { Platform } from "react-native"
+import colors from "./colors"
+
+export default{
+    colors,
+    text:{
+        fontSize: 18,
+        color: colors.dark,
+        ...Platform.select({
+            ios: {
+                fontSize: 20,
+                fontFamily: "Avenir"
+            },
+            android: {
+                fontSize: 18,
+                fontFamily: "Roboto"
+            }
+            })
+    }
+}
